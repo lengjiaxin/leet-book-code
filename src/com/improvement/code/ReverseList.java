790f8edc81ed0d55200a53335714ed68aab6a564
冷家鑫 <690338008@qq.com>
2021-07-31 18:43:12 +0800

    链表反转 添加备注说明

--- src/com/improvement/code/ReverseList.java
+++ src/com/improvement/code/ReverseList.java
@@ -6,16 +6,23 @@ package com.improvement.code;
  * @date: 2021/7/31 17:18
  */
 public class ReverseList {
-
-    public ListNode reverseList(ListNode head) {
-        // 后继节点
-        ListNode prev = null;
-        ListNode curr = head;
+    /**
+     * 迭代方法
+     * 1 -> 2 -> 3 -> 4 -> null
+     * null <- 1 <- 2 <- 3 <- 4
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseListIterative(ListNode head) {
+        ListNode prev = null; //前指针节点
+        ListNode curr = head; //当前指针节点
+        //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
         while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+            ListNode nextTemp = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
+            curr.next = prev; //将当前节点指向它前面的节点
+            prev = curr; //前指针后移
+            curr = nextTemp; //当前指针后移
         }
         return prev;
     }
